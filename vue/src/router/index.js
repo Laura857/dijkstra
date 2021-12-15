49c86017927d59b98f6017a25b557c825c8dc89a
Laura <laura@MacBook-Pro-de-Laura-3.home>
2021-12-15 12:55:09 +0100

    Puissance 4

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Pokemon from '@/components/Pokemon'
-import Threejs from'@/components/Threejs'
+import Threejs from '@/components/Threejs'
+import Power4 from '@/components/Power4'
+
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +17,11 @@ export default new Router({
       path: '/three/',
       name: 'Threejs',
       component: Threejs
+    },
+    {
+      path: '/power4/',
+      name: 'Power4',
+      component: Power4
     }
 
   ]
